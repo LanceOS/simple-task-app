354b94acf4f3e6fa9868c91348ec04429323e416
Lance <lancehemphill2000@gmail.com>
2025-08-24 14:55:32 -0400

    wrote playwright test for groups page

---
+++ e2e/group_tests/get_groups.test.ts
@@ -0,0 +1,38 @@
+import test, { expect } from 'playwright/test';
+
+test('test_group_data_propagation', async ({ page }) => {
+	await page.route('**/groups', async (route) => {
+		const json = [
+			{
+				id: 10,
+				groupName: 'Test Group',
+				description: 'This is a test',
+				ownerId: 10,
+				type: 'task_group',
+				deleted: false,
+				createdAt: new Date(),
+				updatedAt: new Date()
+			},
+			{
+				id: 11,
+				groupName: 'Test Group 2',
+				description: 'This is a test 2',
+				ownerId: 10,
+				type: 'task_group',
+				deleted: false,
+				createdAt: new Date(),
+				updatedAt: new Date()
+			}
+		];
+
+		await route.fulfill({ json });
+	});
+
+	await page.goto('/groups');
+
+	await expect(page.getByText('Test Group')).toBeVisible();
+	await expect(page.getByText('This is a test')).toBeVisible();
+
+	await expect(page.getByText('Test Group 2')).toBeVisible();
+	await expect(page.getByText('This is a test 2')).toBeVisible();
+});
