f300395295e51d683d67b6db22cc2dfdd9d2dd8a
Lance <lancehemphill2000@gmail.com>
2025-09-01 00:27:47 -0400

    added journal to onboarding route, fixed response error

--- src/routes/onboarding/+server.ts
+++ src/routes/onboarding/+server.ts
@@ -1,18 +1,18 @@
 import { auth } from '$lib/auth';
 import { HttpError, ResponseHandler } from '$lib/server/helpers/ResponseHandler.helper';
+import { GetUser } from '$lib/server/helpers/UserCheck.helper.js';
+import { journalService } from '$lib/server/services/Journalist.serverutil.js';
 import { UserServant } from '$lib/server/services/User.serverutil.js';
+import type { RequestEvent, RequestHandler } from './$types';
 
-export const POST = async ({ request }) => {
+export const POST: RequestHandler = async ({ request }: RequestEvent) => {
 	try {
 		const body = await request.json();
 
-		console.log(body);
 
-		const session = await auth.api.getSession({
-			headers: request.headers
-		});
+		const user = await GetUser(request)
 
-		if (!session?.user) {
+		if (!user) {
 			return ResponseHandler.jsonResponse('User must be signed in to onboard!', 401);
 		}
 
@@ -20,11 +20,18 @@ export const POST = async ({ request }) => {
 			return ResponseHandler.jsonResponse('Missing required data!', 400);
 		}
 
-		const response = await UserServant.setUsername(session.user.id, body);
+		await UserServant.setUsername(user.id, body);
+		
+		await journalService.writeJournal({
+			action: "Assigned Name",
+			description: "User has associated a name to their account.",
+			metadata: {
+				assignedName: body,
+				userId: user.id
+			}
+		})
 
-		console.log(response);
-
-		return ResponseHandler.jsonResponse(response, 200);
+		return ResponseHandler.jsonResponse("Name successfully assigned!", 200);
 	} catch (error: any) {
 		if (error instanceof HttpError) {
 			return ResponseHandler.jsonResponse(error.message, error.status);
