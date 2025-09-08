4d08c0ca1c6e38ca884c9eca7e2a4cc0924727c8
Lance <lancehemphill2000@gmail.com>
2025-09-07 20:10:27 -0400

    reset to last head, removed producer server

---
+++ RabbitMQ-Consumer/drizzle.config.ts
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import { defineConfig, type Config } from 'drizzle-kit';
+
+const databaseUrl = `postgresql://${process.env.PRIVATE_POSTGRES_USER!}:${process.env.PRIVATE_POSTGRES_PASSWORD!}@${process.env.PRIVATE_POSTGRES_URL}/${process.env.PRIVATE_POSTGRES_DB!}`;
+
+export default defineConfig({
+  out: './drizzle', 
+  schema: './src/lib/server/schemas/*.ts', 
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: databaseUrl, 
+  },
+}) satisfies Config;
